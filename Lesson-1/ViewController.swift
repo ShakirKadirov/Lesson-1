cc6227992eab2a6e6bc9cb4a582121b3fd2a3482
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-08 22:12:08 +0600

    Initial Commit

---
+++ Lesson-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-1
+//
+//  Created by Shakir Kadirov on 08.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
